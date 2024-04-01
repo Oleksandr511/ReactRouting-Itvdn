ee5f4488ec2fba4257599ed3b38ea0668b619020
Oleksandr <fjvffr@gmail.com>
2024-04-01 12:18:50 +0300

    Add modal window

--- src/Pages/Users/Users.jsx
+++ src/Pages/Users/Users.jsx
@@ -1,19 +1,28 @@
 import { Link } from "react-router-dom"
+import { useState } from "react"
+import '../../components/Modal/ModalWnd'
+import ModalWnd from "../../components/Modal/ModalWnd"
 
 export default function Users() {
+    const [modalState, setModalState] = useState(false);
     return (
         <div className="MAIN">
             <h1>USERS</h1>
+
+            <ModalWnd call={modalState} onDestroy={() => setModalState(false)} />
             <ul>
                 <li>
                     <Link to='/users/walles'>
                         John Walles
                     </Link>
+                    <button onClick={() => setModalState(true)}>X</button>
                 </li>
                 <li>
                     <Link to='/users/beanit'>
                         Jim Beanit
                     </Link>
+                    <button onClick={() => setModalState(true)}>X</button>
+
                 </li>
             </ul>
         </div>
---
+++ src/components/Modal/ModalWnd.jsx
@@ -0,0 +1,27 @@
+import './ModalWnd.css';
+
+export default function ModalWnd({ call, onDestroy }){
+
+    if(!call){
+        return null;
+    }
+
+    const closeWnd = (event) =>{
+        if(event.target.className === 'modal'){
+            onDestroy();
+        }
+    }
+
+    return (
+        <div onClick={closeWnd} className='modal'>
+            <div className="modal-content">
+                <i onClick={onDestroy} className='close'>X</i>
+                <h1>Видалити запис?</h1>
+                <div className="btns">
+                    <button className='accept'>Так, видалити</button>
+                    <button onClick={onDestroy} className='reject'>Ні, залишити</button>
+                </div>
+            </div>
+        </div>
+    );
+}
